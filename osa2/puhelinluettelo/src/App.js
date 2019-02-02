13fc0a7a9cd4a552820ce92d8d95a11f58f6c75d
Anna Piirainen <anna.r.piirainen@helsinki.fi>
2019-02-02 15:13:30 +0200

    Osa2, teht6

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const rows = () => (
+    persons.map(person => <p key={person.name}>{person.name}</p>)
+    
+  )
+
+  return (
+    <div>
+      <h2>Puhelinluettelo</h2>
+      <form onSubmit={addName}>
+        <div>
+          nimi: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">lisää</button>
+        </div>
+      </form>
+      <h2>Numerot</h2>
+      {rows()}
+    </div>
+  )
+
+}
+
+export default App
