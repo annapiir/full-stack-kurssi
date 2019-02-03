324ed9663663bd7aecf90130050d33a3177b43f5
Anna Piirainen <anna.r.piirainen@helsinki.fi>
2019-02-03 12:10:11 +0200

    Osa2, teht9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [ persons, setPersons] = useState(
+    [{ name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Martti Tienari', number: '040-123456' },
+    { name: 'Arto Järvinen', number: '040-123456' },
+    { name: 'Lea Kutvonen', number: '040-123456' }]
+  ) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ showName, setShowName ] = useState('')
+
 
   const addName = (event) => {
     event.preventDefault()
@@ -13,11 +19,13 @@ const App = () => {
       window.alert(`${newName} on jo luettelossa`)
     } else {
       const nameObject = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
 
       setPersons(persons.concat(nameObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -25,23 +33,38 @@ const App = () => {
     setNewName(event.target.value)
   }
 
-  const rows = () => (
-    persons.map(person => <p key={person.name}>{person.name}</p>)
-    
-  )
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleShowNameChange = (event) => {
+    setShowName(event.target.value)
+  }
+
+  const rows = () => {
+    const showed = persons.filter(person => person.name.toUpperCase().includes(showName.toUpperCase()))
+
+    return (
+      showed.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+    )
+  }
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
+      <div>
+        Rajaa näytettäviä:  
+        <input value={showName} onChange={handleShowNameChange} />
+      </div>
+      <h3>Lisää uusi</h3>
       <form onSubmit={addName}>
-        <div>
-          nimi: <input value={newName} onChange={handleNameChange}/>
-        </div>
+        <div>Nimi: <input value={newName} onChange={handleNameChange}/></div>
+        <div>Numero: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div>
           <button type="submit">lisää</button>
         </div>
       </form>
-      <h2>Numerot</h2>
+      <h3>Numerot</h3>
       {rows()}
     </div>
   )
