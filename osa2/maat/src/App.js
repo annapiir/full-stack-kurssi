965d877e1b8a98590ff8eba579a4c8af309e2873
Anna Piirainen <anna.r.piirainen@helsinki.fi>
2019-02-03 20:03:39 +0200

    Osa2, teht12

---
+++ osa2/maat/src/App.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Countries = ({countries, showCountries}) => {
+  const showed = countries.filter(country => 
+    country.name.toUpperCase().includes(showCountries.toUpperCase()))
+    
+
+  if (showed.length > 10) {
+
+    return (<div>Too many matches, specify another filter</div>)
+
+  } else if (showed.length === 1) {
+
+    const country = showed[0]
+    return (<Country country={country} />)
+
+  } else {
+
+    return (
+      showed.map(country => <div key={country.name}>{country.name}</div> )
+    )
+  }
+}
+
+const Country = ({country}) => {
+  const languages = country.languages.map(language => <li>{language.name}</li>)
+
+  return (
+    <>
+      <h1>{country.name}</h1>
+      <div>Capital: {country.capital}</div>
+      <div>Population: {country.population}</div>
+      <h2>Languages</h2>
+      <ul>
+        {languages}
+      </ul>
+      <img src={country.flag} alt={country.name} width="300" heigth="200" />
+    </>
+  )
+}
+
+const App = () => {
+  const [ countries, setCountries ] = useState([])
+  const [ showCountries, setShowCountries ] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+      setCountries([].slice.call(response.data))
+    })
+  }, [])
+
+
+  const handleShowCountriesChange = (event) => {
+    setShowCountries(event.target.value)
+  }
+
+  return (
+    <div>
+      <p>
+        Find countries: 
+        <input value={showCountries} onChange={handleShowCountriesChange} />  
+      </p>
+      <Countries countries={countries} showCountries={showCountries} />
+    </div>
+  )
+}
+
+export default App;
