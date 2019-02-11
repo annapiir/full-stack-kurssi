40a18cbbc1d827cd764c520c76110cbf576ceadd
Anna Piirainen <anna.r.piirainen@helsinki.fi>
2019-02-11 22:14:06 +0200

    Osa3, teht19

--- osa3/puhlbackend/index.js
+++ osa3/puhlbackend/index.js
@@ -44,7 +44,7 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
   
   if (body.name === undefined) {
@@ -59,6 +59,7 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson.toJSON())
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -96,6 +97,8 @@ const errorHandler = (error, request, response, next) => {
   
     if (error.name === 'CastError' && error.kind == 'ObjectId') {
       return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
     } 
   
     next(error)
