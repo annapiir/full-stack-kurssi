ad179cc8ce3811383fa794b5d0b675391e047b5f
Anna Piirainen <anna.r.piirainen@helsinki.fi>
2019-02-14 22:01:01 +0200

    Osa4, teht2

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,60 +1,9 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
-
+const app = require('./app') // varsinainen Express-sovellus
 const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
 
+const server = http.createServer(app)
 
-  
-let PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
-  
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
-
-
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
