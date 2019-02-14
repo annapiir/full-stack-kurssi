24ca05d29aa64f1fc5541f8c213fdaff9d622dac
Anna Piirainen <anna.r.piirainen@helsinki.fi>
2019-02-14 21:41:26 +0200

    Osa4, teht1

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,9 +1,19 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+
+
+  
+let PORT = process.env.PORT
+let MONGODB_URI = process.env.MONGODB_URI
+  
 
 const blogSchema = mongoose.Schema({
   title: String,
@@ -14,8 +24,14 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -38,7 +54,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
