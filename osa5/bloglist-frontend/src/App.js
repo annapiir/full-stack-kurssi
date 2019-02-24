86cec21adc9c571158366ffbce869bfb564a10f9
Anna Piirainen <anna.r.piirainen@helsinki.fi>
2019-02-24 15:40:34 +0200

    Osa5, teht3

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login' 
+import BlogForm from './components/BlogForm';
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
+  const [newTitle, setNewTitle]= useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -50,6 +54,26 @@ const handleLogout = async (event) => {
   setUser(null)
 }
 
+const handleAddBlog = async (event) => {
+  event.preventDefault()
+
+  const createdBlog = await blogService.create({
+    title: newTitle,
+    author: newAuthor,
+    url: newUrl
+  })
+  setBlogs(blogs.concat(createdBlog))
+  setNewTitle('')
+  setNewAuthor('')
+  setNewUrl('')
+
+  return
+}
+
+const handleTitleChange = (event) => setNewTitle(event.target.value)
+const handleAuthorChange = (event) => setNewAuthor(event.target.value)
+const handleUrlChange = (event) => setNewUrl(event.target.value)
+
   if (user === null) {
     return (
       <div>
@@ -81,12 +105,23 @@ const handleLogout = async (event) => {
 
   return (
     <div>
-      <h2>blogs</h2>
+      <h2>Blogs</h2>
       <p>{user.name} logged in</p>
       <p></p>
       <form onSubmit={handleLogout}>
         <button type="submit">logout</button>
       </form>
+      <h2>Create New</h2>
+      <BlogForm
+        handleTitleChange={handleTitleChange}
+        handleAuthorChange={handleAuthorChange}
+        handleUrlChange={handleUrlChange}
+        newTitle={newTitle}
+        newAuthor={newAuthor}
+        newUrl={newUrl}
+        handleAddBlog={handleAddBlog}
+      />
+      <h2>List of Blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
