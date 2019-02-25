8b94f72f39fee50b0da1b4b30d55b4be92c668fd
Anna Piirainen <anna.r.piirainen@helsinki.fi>
2019-02-25 23:28:02 +0200

    Osa5, teht6

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,34 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => setVisible(!visible)}>
+        {blog.title} {blog.author}
+      </div>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <p>
+          {blog.likes} likes <button onCLick={() => console.log('click')}>like</button>
+        </p>
+        <p>
+          Added by {blog.user.name}
+        </p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
