7bd952ad7c1f071ab63511e995749f4a29d83af1
Anna Piirainen <anna.r.piirainen@helsinki.fi>
2019-02-25 20:21:07 +0200

    Osa5, teht14

--- osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'jest-dom/extend-expect'
-import { render, cleanup } from 'react-testing-library'
+import { render, cleanup, fireEvent } from 'react-testing-library'
 import SimpleBlog from './SimpleBlog'
 
 
@@ -58,4 +58,24 @@ test('renders author', () => {
   expect(component.container).toHaveTextContent(
     '1789'
   )
+})
+
+test('pushing like-button twice calls two likes', () => {
+  const blog = {
+    title: 'Reactin testaus on parasta ikinä',
+    author: 'Test Master',
+    likes: 1789
+  }
+
+  const mockHandler = jest.fn()
+
+  const { getByText } = render(
+    <SimpleBlog blog={blog} onClick={mockHandler} />
+  )
+
+  const button = getByText('like')
+  fireEvent.click(button)
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls.length).toBe(2)
 })
